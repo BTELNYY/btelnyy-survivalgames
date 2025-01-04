3d9f4be21d926bc4354768d4cdedd7716ed1ea98
BTELNYY <btelnyy@gmail.com>
2025-01-04 00:32:31 -0600

    ?

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 rem this is used by btelnyy, do not modify
-mvn package &xcopy .\target\*.jar D:\Minecraft\HideAndSeek\plugins /Y
+mvn package &xcopy .\target\*.jar D:\Minecraft\Test\plugins /Y
